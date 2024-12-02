c610e24fdf8df4662f2e3830443dd10aca930d3a
battlelolo <belvederedere@gmail.com>
2024-12-02 18:11:03 +0900

    config

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['i.imgur.com', 'images.unsplash.com'], // 사용할 이미지 도메인들
-  },
-};
-
-export default nextConfig;
