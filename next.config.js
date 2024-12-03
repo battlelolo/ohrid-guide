504351c5ac61230077cc1c340bfba9c3a6e728d0
battlelolo <belvederedere@gmail.com>
2024-12-04 02:17:14 +0900

    pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['i.imgur.com', 'images.unsplash.com','th.bing.com','sl.bing.net'],
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['i.imgur.com', 'images.unsplash.com','th.bing.com','sl.bing.net'],
+//   },
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    // 기타 PWA 설정
+  },
+  images: {
+    domains: ['i.imgur.com', 'images.unsplash.com', 'th.bing.com', 'sl.bing.net'],
+  },
+});
